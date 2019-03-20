39da848da01e6c5729e3130d4d5f7a4f7b564b7b
sofu0185 <sofu0185@edu.easj.dk>
2019-03-20 12:58:55 +0100

    base klasse

---
+++ src/js/kaffe.ts
@@ -0,0 +1,8 @@
+export abstract class Kaffe {
+    rabat: number;
+    constructor(rabat: number){
+        this.rabat = rabat;
+    }
+    abstract pris(): number;
+    styrke(): string { return "Mild" }
+}
