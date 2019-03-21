7e4bad58a5777e0f9d2dcab442b6715eaae955b4
sofu0185 <sofu0185@edu.easj.dk>
2019-03-21 16:07:57 +0100

    menu kort i html

--- src/js/index.ts
+++ src/js/index.ts
@@ -9,6 +9,35 @@ kaffer.push(new Cortado());
 kaffer.push(new Latte());
 kaffer.push(new SortKaffe(4));
 
+let list: HTMLElement = document.getElementById('menuKort');
+
 kaffer.forEach(element => {
     console.log(element.kaffeInfo());
+
+    let newKaffe: HTMLElement = document.createElement('li');
+    newKaffe.setAttribute('class', 'kaffe')
+
+    let kaffeType: HTMLElement = document.createElement('span');
+    kaffeType.setAttribute('class', 'kaffeType');
+    kaffeType.innerText = 'Type: ' + element.kaffeDrik();
+
+    let kaffePris: HTMLElement = document.createElement('span');
+    kaffePris.setAttribute('class', 'kaffePris');
+    kaffePris.innerText = 'Pris: ' + element.pris().toString();
+
+    let kaffeStyrke: HTMLElement = document.createElement('span');
+    kaffeStyrke.setAttribute('class', 'kaffeStyrke');
+    kaffeStyrke.innerText = 'Styrke: ' + element.styrke();
+
+    let kaffeRabat: HTMLElement = document.createElement('span');
+    kaffeRabat.setAttribute('class', 'kaffeRabat');
+    kaffeRabat.innerText = 'Rabat: ' + element.rabat.toString();
+
+    newKaffe.appendChild(kaffePris);
+    newKaffe.appendChild(kaffeStyrke);
+    newKaffe.appendChild(kaffeType);
+    newKaffe.appendChild(kaffeRabat);
+
+    list.appendChild(newKaffe);
 });
+
